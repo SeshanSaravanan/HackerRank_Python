e58cbb9cb179cfb42fae737bf601a97e77946fac
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-05-16 09:26:41 +0530

    Create String_split_and_join.py

---
+++ String/String_split_and_join.py
@@ -0,0 +1,45 @@
+In Python, a string can be split on a delimiter.
+
+Example:
+
+>>> a = "this is a string"
+>>> a = a.split(" ") # a is converted to a list of strings. 
+>>> print a
+['this', 'is', 'a', 'string']
+Joining a string is simple:
+
+>>> a = "-".join(a)
+>>> print a
+this-is-a-string 
+Task
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Function Description
+
+Complete the split_and_join function in the editor below.
+
+split_and_join has the following parameters:
+
+string line: a string of space-separated words
+Returns
+
+string: the resulting string
+Input Format
+The one line contains a string consisting of space separated words.
+
+Sample Input
+
+this is a string   
+Sample Output
+
+#this-is-a-string
+
+******************************************************************************************************************************
+Solution: 
+  
+def split_and_join(line):
+    # write your code here
+    Output = line.split();
+    Output = "-".join(Output)
+    return Output;
+if __name__ == '__main__':
