6e5a9a03a9fe08363d52ffbd3a9865527557976d
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-21 13:15:47 +0530

    Create main.py

---
+++ Introduction/Loops/main.py
@@ -0,0 +1,13 @@
+  
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n):
+        print(i ** 2)
+        
+ALTERNATIVE: 
+  
+if __name__ == '__main__':
+    n = int(input())
+for number in range(0,n):    
+    square_number = number * number
+    print(square_number) 
